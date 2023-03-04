31280aa246a1be67e37b6ec7868d0b515f57fcf7
Zondy The <70164512+z0ndy@users.noreply.github.com>
2023-03-04 01:34:02 +0000

    Added Dynamic Data and API Route

--- app.py
+++ app.py
@@ -1,10 +1,47 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
+from flask import jsonify
 
 app = Flask(__name__)
 
+FRIENDS= [
+  {
+    "name":"Arun",
+    "dank": "yes",
+    "good": "yes"
+  },
+  {
+    "name":"Chetan",
+    "dank": "yes",
+    "good": "yes"
+  },
+  {
+    "name":"Madhu",
+    "dank": "yes",
+    "good": "yes"
+  },
+  {
+    "name":"Venu",
+    "dank": "yes",
+    "good": "yes"
+  },
+  {
+    "name":"Abhi",
+    "dank": "yes",
+    "good": "yes"
+  }
+  
+]
+  
+  
+
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html',
+                          friends = FRIENDS)
+@app.route("/api/friends")
+def json():
+  return jsonify(FRIENDS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
